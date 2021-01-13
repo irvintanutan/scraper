fc95a2b1055e5a43d5b3f31232b0374bd07487c1
Irvin Tanutan <itanutan@placester.com>
2021-01-13 21:43:08 +0800

    Controllers and Service

--- src/main/java/com/example/yelp/controller/YelpController.java
+++ src/main/java/com/example/yelp/controller/YelpController.java
@@ -1,5 +1,21 @@
 package com.example.yelp.controller;
 
-public class YelpController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.yelp.service.YelpService;
+
+@RestController
+public class YelpController {
+		
+	@Autowired
+	YelpService service;
+	
+	@GetMapping("/client")
+	public String getAllClient() {
+		
+		return this.service.getResponse();
+	}
+	
 }
---
+++ src/main/java/com/example/yelp/service/YelpService.java
@@ -0,0 +1,14 @@
+package com.example.yelp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class YelpService {
+
+	public String getResponse() {
+		
+		return "Something";
+		
+	}
+	
+}
