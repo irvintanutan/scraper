2a0e12f301b9fcf02d00df57784cfa7f353272ff
Irvin Tanutan <itanutan@placester.com>
2021-01-14 12:11:49 +0800

    [update] - minor changes

--- src/main/java/com/example/yelp/controller/YelpController.java
+++ src/main/java/com/example/yelp/controller/YelpController.java
@@ -1,9 +1,13 @@
 package com.example.yelp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.Review;
 import com.example.yelp.service.YelpServiceImpl;
 
 @RestController
@@ -12,10 +16,9 @@ public class YelpController {
 	@Autowired
 	YelpServiceImpl service;
 	
-	@GetMapping("/client")
-	public String getAllClient() {
-		
-		return this.service.getResponse();
+	@GetMapping("/{url}")
+	public List<Review> getAllClient(@PathVariable String url) { 
+		return this.service.getAllReviewByRestaurant(url);
 	}
 	
 }
